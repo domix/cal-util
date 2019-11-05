493dff11441e629d2d51a372bd338b21cbddaf08
Domingo Suarez Torres <domingo.suarez@gmail.com>
2019-11-05 12:38:44 -0600

    wip for file loading

--- src/main/java/cal/util/service/CalendarService.java
+++ src/main/java/cal/util/service/CalendarService.java
@@ -1,7 +1,29 @@
 package cal.util.service;
 
-import javax.inject.Singleton;
+import lombok.extern.slf4j.Slf4j;
+import org.yaml.snakeyaml.Yaml;
 
+import javax.inject.Singleton;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+@Slf4j
 @Singleton
 public class CalendarService {
+  private String tenantsFile;
+  List<String> orgs;
+
+  public void load() {
+    log.info("Loading [{}]", tenantsFile);
+    Yaml yaml = new Yaml();
+    String content = null;
+    try {
+      content = Files.readString(Paths.get(tenantsFile));
+    } catch (IOException e) {
+      throw new RuntimeException("Valio...");
+    }
+    orgs = yaml.load(content);
+  }
 }
